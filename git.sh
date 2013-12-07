2b11bb5d76fe2c1301a55b8796a85f9f7e7f7628
atwoodunix <wangatwood@gmail.com>
2013-12-07 19:35:44 +0000

    modify

---
+++ git.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm ../git_bak/* -rf
+mv ../wangjason.github.io/.git ../git_bak/
+rm ../wangjason.github.io/* -rf
+mv _site/* ../wangjason.github.io/
+mv ../git_bak/.git ../wangjason.github.io/
